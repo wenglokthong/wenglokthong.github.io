956bafc7d99ba1db9ce13785d8b77ed3c5dc4ad7
Max <wenglokthong@gmail.com>
2022-05-03 16:27:44 +0800

    changed to hashrouter

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,10 @@
 ///entry point///
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import MyRoutes from "./routes.js";
 
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<BrowserRouter>{MyRoutes}</BrowserRouter>);
+root.render(<HashRouter>{MyRoutes}</HashRouter>);
