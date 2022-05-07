0b3eedf8008485b71e6a329b709c804b33c4514b
Max <wenglokthong@gmail.com>
2022-05-08 00:23:47 +0800

    removed hot loader

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,4 @@
 ///entry point///
-import { hot } from "react-hot-loader/root";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
@@ -12,5 +11,3 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const App = () => <HashRouter>{MyRoutes}</HashRouter>;
 
 root.render(<App />);
-
-export default hot(App);
